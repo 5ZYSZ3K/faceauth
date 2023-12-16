c0f39dbe6d04a1faf61db3f285fbbbc0e96ad533
5ZYSZ3K <paw.por@wp.pl>
2023-12-16 20:37:05 +0100

    login and register

--- faceauth/view.py
+++ faceauth/view.py
@@ -1,6 +1,9 @@
 import json
 
-from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views import View
 
@@ -8,8 +11,15 @@ from django.views import View
 class FaceAuthView(View):
     def post(self, request):
         data = json.loads(request.body)
+        print(data)
+        try:
+            user = User.objects.create_user(username=data['login'], password=data["password"])
+        except IntegrityError:
+            return HttpResponse('Unauthorized', status=401)
+        login(request, user)
+
         return JsonResponse(
-            {"message": "Received POST request with {} login".format(data["login"])}
+            {"message": "Hello {}".format(user.username)}
         )
 
     def get(self, request):
